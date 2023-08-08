f9cbd5779989f4040f1681299f15d88767745bfa
Sherwin Abraham <sherwinabraham@gmail.com>
2023-08-08 12:54:19 -0500

    Added roster.

--- db/schema.sql
+++ db/schema.sql
@@ -9,7 +9,17 @@ CREATE TABLE departments(
 
 CREATE TABLE roles(
     id INT NOT NULL,
-    title VARCHAR(255) NOT NULL
-    department VARCHAR(255) NOT NULL
+    title VARCHAR(255) NOT NULL,
+    department VARCHAR(255) NOT NULL,
     salary INT NOT NULL
+);
+
+CREATE TABLE roster(
+    id INT NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    title VARCHAR(30) NOT NULL,
+    department VARCHAR(30) NOT NULL,
+    salary INT NOT NULL,
+    manager VARCHAR(30)
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -25,3 +25,21 @@ VALUES
     (13, "Receptionist", "Front Desk", 35000);
 
 SELECT * FROM roles;
+
+INSERT INTO roster (id, first_name, last_name, title, department, salary, manager)
+VALUES
+    (1, "J. Jonah", "Jameson", "Sales Lead", "Sales", 150000, ""),
+    (2, "Jenny", "Jones", "HR Manager", "Human Resources", 100000, ""),
+    (3, "Jamaal", "Wilkes", "Sr. Software Engineer", "Information Technology", 80000, "Domingo Chavez"),
+    (4, "Giuseppe", "Garibaldi", "Jr. Software Engineer", "Information Technology", 50000, "Domingo Chavez"),
+    (5, "Joan", "Jett", "Receptionist", "Front Desk", 35000, "Vijaya Patel"),
+    (6, "John", "Smith", "HR Clerk", "Human Resources", 45000, "Jenny Jones"),
+    (7, "Vijaya", "Patel", "Head Receptionist", "Front Desk", 50000, ""),
+    (8, "Domingo", "Chavez", "Lead Engineer", "Information Technology", 120000, ""),
+    (9, "Bernie", "Madoff", "Accounting Manager", "Accounting", 100000, ""),
+    (10, "Canine", "St. Bernard", "Sr. Accountant", "Accounting", 90000, "Bernie Madoff"),
+    (11, "Lois", "Lane", "Sr. Salesman", "Sales", 95000, "J. Jonah Jameson"),
+    (12, "Gil", "Chesterton", "Jr. Accountant", "Accounting", 60000, "Bernie Madoff"),
+    (13, "Johnny", "Chan", "Jr. Salesman", "Sales", 50000, "J. Jonah Jameson");
+
+SELECT * FROM roster;
