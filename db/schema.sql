2a942f0ecfb09fdad833068dcbdcd9a48da1c0d9
Sherwin Abraham <sherwinabraham@gmail.com>
2023-08-08 11:44:59 -0500

    Added sql tables.

--- db/schema.sql
+++ db/schema.sql
@@ -2,7 +2,14 @@ DROP DATABASE IF EXISTS employee_db;
 CREATE DATABASE employee_db;
 USE employee_db;
 
-CREATE TABLE departments (
-    id NOT NULL,
-    name VARCHAR(255) NOT NULL,
+CREATE TABLE departments(
+    id INT NOT NULL,
+    name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE roles(
+    id INT NOT NULL,
+    title VARCHAR(255) NOT NULL,
+    department VARCHAR(255) NOT NULL,
+    salary INT NOT NULL
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -4,6 +4,24 @@ VALUES
     (2, "Information Technology"),
     (3, "Accounting"),
     (4, "Human Resources"),
-    (5, "Front Desk")
+    (5, "Front Desk");
 
 SELECT * FROM departments;
+
+INSERT INTO roles (id, title, department, salary)
+VALUES
+    (1, "Sales Lead", "Sales", 150000),
+    (2, "Sr. Saleman", "Sales", 95000),
+    (3, "Jr. Saleman", "Sales", 50000),
+    (4, "Lead Engineer", "Information Technology", 120000),
+    (5, "Sr. Software Engineer", "Information Technology", 80000),
+    (6, "Jr. Software Engineer", "Information Technology", 50000),
+    (7, "Accounting Manager", "Accounting", 100000),
+    (8, "Sr. Accountant", "Accounting", 90000),
+    (9, "Jr. Accountant", "Accounting", 60000),
+    (8, "HR Manager", "Human Resources", 100000),
+    (9, "HR Clerk", "Human Resources", 45000),
+    (10, "Head Receptionist", "Front Desk", 50000),
+    (11, "Receptionist", "Front Desk", 35000);
+
+SELECT * FROM roles;
